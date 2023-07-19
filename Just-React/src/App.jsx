081b69aaf691e0610f02b5eaa8d03cc7b533f5af
GodzK <64467457+GodzK@users.noreply.github.com>
2023-07-19 21:50:34 +0700

    1day
    state and function of change note
    สกัดตัวเเปร event.target
     //เอาdataมาเเปะในonchange funtionนี้เป็นfunctionตัวกลางจากnameกับvalueที่เพิ่งสร้าง

---
+++ Just-React/src/App.jsx
@@ -0,0 +1,52 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [note, setNote] = useState({
+    content: "",
+    author: "",
+  });
+  //เก็บdataเป็นobject
+
+  //เอาdataมาเเปะในonchange funtionนี้เป็นfunctionตัวกลาง
+  function onNoteValueChange(event) {
+    const { name, value } = event.target; //สกัดตัวเเปร event.target จากnameกับvalueที่เพิ่งสร้าง
+    setNote((prevnote) => {
+      return {
+        ...prevnote, //เอาcontentเเละauthorที่เราสร้างมาเยะเเยะหุบเหลือเเค่...prevnote
+        [name]: value,
+      };
+    });
+  }
+
+  return (
+    <section className="app-section">
+      <div className="app-container">
+        <h3>Jookroo</h3>
+        <p>
+          <textarea 
+          rows = "3"
+            type="text"
+            placeholder="ความในใจ"
+            value={note.content}
+            name="content"
+            onChange={onNoteValueChange} //ส่งข้อมูลชื่อOnnote valueไปในfunction
+          />
+        </p>
+        <p>
+          <input
+            type="text"
+            placeholder="ลงชื่อ"
+            name="author"
+            value={note.author}
+            onChange={onNoteValueChange}
+          />
+        </p>
+      </div>
+    </section>
+  );
+}
+
+export default App;
